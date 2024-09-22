2a1e85404a84f5ced41c3b874c1686c7e21353a3
VIPIN KUSHWAHA <kushwahavipin786@gmail.com>
2024-09-22 11:32:42 +0530

    update

--- src/Landing_page.js
+++ src/Landing_page.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import './Landing_page.css';
-import Landing_page_ellipse_468 from "./assets/img_ellipse_468.png";
-import Landing_page_group_1 from "./assets/img_group_1.svg"
-import Landing_page_analog_landscap from "./assets/img_analog_landscap.png"
-import Landing_page_happy_asian_man from "./assets/img_happy_asian_man.png"
-import Landing_page_contrast from "./assets/img_contrast.svg"
-import Landing_page_man_safety_equipment_work from "./assets/img_man_safety_equipment_work.png"
-import Landing_page_success from "./assets/img_success.svg"
-import Landing_page_header_logo from "./assets/img_header_logo.svg"
-import Landing_page_woman_hands_hol from "./assets/img_woman_hands_hol.png"
-import Landing_page_overflow_menu from "./assets/img_overflow_menu.svg"
-import Landing_page_success_teal_900 from "./assets/img_success_teal_900.svg";
-import Landing_page_close from "./assets/img_close.svg";
-import Landing_page_image_64 from "./assets/img_image_64.png";
-import Landing_page_image_54 from "./assets/img_image_54.png";
-import Landing_page_trending_up from "./assets/img_trending_up.svg";
-import Landing_page_image_62_1_2 from "./assets/img_image_62_1_2.png";
-import Landing_page_image_55 from "./assets/img_image_55.png";
-import Landing_page_whatsapp_image_2023_10_06 from "./assets/img_whatsapp_image_2023_10_06.png";
-import Landing_page_image_59 from "./assets/img_image_59.png";
+import img_ellipse_468 from "./assets/img_ellipse_468.png";
+import img_group_1 from "./assets/img_group_1.svg"
+import img_analog_landscap from "./assets/img_analog_landscap.png"
+import img_happy_asian_man from "./assets/img_happy_asian_man.png"
+import img_contrast from "./assets/img_contrast.svg"
+import img_man_safety_equipment_work from "./assets/img_man_safety_equipment_work.png"
+import img_success from "./assets/img_success.svg"
+import img_header_logo from "./assets/img_header_logo.svg"
+import img_woman_hands_hol from "./assets/img_woman_hands_hol.png"
+import img_overflow_menu from "./assets/img_overflow_menu.svg"
+import img_success_teal_900 from "./assets/img_success_teal_900.svg";
+import img_close from "./assets/img_close.svg";
+import img_image_64 from "./assets/img_image_64.png";
+import img_image_54 from "./assets/img_image_54.png";
+import img_trending_up from "./assets/img_trending_up.svg";
+import img_image_62_1_2 from "./assets/img_image_62_1_2.png";
+import img_image_55 from "./assets/img_image_55.png";
+import img_whatsapp_image_2023_10_06 from "./assets/img_whatsapp_image_2023_10_06.png";
+import img_image_59 from "./assets/img_image_59.png";
 
 
 
@@ -28,11 +28,83 @@ const Landing_page = () => {
   return (
     <>
     {/* <!-- main content section --> */}
-    <div className="macbook-pro-14">
+    
       <div className="main-layout">
-      {/* <!-- navigation header section --> */}
+        {/* <!-- hero banner section --> */}
+        <div className="primary-column">
+          <div className="feature-row">
+            <div className="whatsapp-column">
+              <h1 className="section__title ui heading size-heading3xl">
+                WhatsApp Learning to Transform your<br />Delivery Workforce
+              </h1>
+              <p className="section__subtitle ui text size-textxl">
+                Unlock their True Potential with AI-Driven Vernacular Training Content and WhatsApp learning.
+              </p>
+              <div className="cta-row">
+                <button className="section__button--primary ui button blue_a700 size-xs fill">Try chaabi for Free</button>
+                <button className="section__button--secondary ui button white_a700 size-xs fill">View Demo</button>
+              </div>
+            </div>
+            <div className="image-stack">
+              <img src={img_ellipse_468} alt="Hero Image" className="section__image--featured" />
+              <div className="view-row">
+                <div className="section__view--default"></div>
+                <div className="section__view--alternate"></div>
+              </div>
+              <div className="section__view--additional"></div>
+              <div className="content-row">
+                <div className="success-column">
+                  <img src={img_group_1} alt="Success Image" className="section__image--thumbnail" />
+                  <div className="analog-landscape-row">
+                    <div className="contrast-stack">
+                      <div className="analog-landscape-column">
+                        <div className="analog-landscape-stack">
+                          <img
+                            src={img_analog_landscap}
+                            alt="Analog Landscape Image"
+                            className="section__image--landscape"
+                          />
+                          <img
+                            src={img_happy_asian_man}
+                            alt="Happy Asian Man Image"
+                            className="section__image--portrait"
+                          />
+                        </div>
+                      </div>
+                      <div className="contrast-column">
+                        <div className="contrast-row">
+                          <img
+                            src={img_contrast}
+                            alt="Contrast Image"
+                            className="section__image--contrast"
+                          />
+                          <h2 className="section__text--intro-1 ui heading size-texts">Introduction to vehicle training</h2>
+                        </div>
+                        <img
+                          src={img_man_safety_equipment_work}
+                          alt="Safety Equipment Image"
+                          className="section__image--safety"
+                        />
+                      </div>
+                      <div className="success-row">
+                        <img
+                          src={img_success}
+                          alt="Success Image One"
+                          className="section__image--success"
+                        />
+                        <h3 className="section__text--reduced-1 ui heading size-texts">Reduced Delivery Time</h3>
+                      </div>
+                    </div>
+                    <img src={img_group_1} alt="Group Image Two" className="section__image--group" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* <!-- navigation header section --> */}
           <header className="header">
-            <img src={Landing_page_header_logo} alt="Logo" className="header__logo" />
+            <img src={img_header_logo} alt="Logo Image" className="header__logo" />
             <ul className="header__menu">
               <li>
                 <a href="#">
@@ -51,79 +123,6 @@ const Landing_page = () => {
               </li>
             </ul>
           </header>
-        {/* <!-- hero banner section --> */}
-        <div className="primary-column">
-          <div className="feature-row">
-            <div className="whatsapp-column">
-              <h1 className="section__title ui heading size-heading3xl">
-                WhatsApp Learning to Transform your<br />Delivery Workforce
-              </h1>
-              <p className="section__subtitle ui text size-textxl">
-                Unlock their True Potential with AI-Driven Vernacular Training Content and WhatsApp learning.
-              </p>
-              <div className="cta-row">
-                <button className="section__button--primary ui button blue_a700 size-xs fill">Try chaabi for Free</button>
-                <button className="section__button--secondary ui button white_a700 size-xs fill">View Demo</button>
-              </div>
-            </div>
-            <div className="image-stack">
-              <img src={Landing_page_ellipse_468} alt="Hero" className="section__image--featured" />
-              <div className="view-row">
-                <div className="section__view--default"></div>
-                <div className="section__view--alternate"></div>
-              </div>
-              <div className="section__view--additional"></div>
-              <div className="content-row">
-                <div className="success-column">
-                  <img src={Landing_page_group_1} alt="Success Image" className="section__image--thumbnail" />
-                  <div className="analog-landscape-row">
-                    <div className="contrast-stack">
-                      <div className="analog-landscape-column">
-                        <div className="analog-landscape-stack">
-                          <img
-                            src={Landing_page_analog_landscap}
-                            alt="Analog Landscape "
-                            className="section__image--landscape"
-                          />
-                          <img
-                            src={Landing_page_happy_asian_man}
-                            alt="Happy Asian Man "
-                            className="section__image--portrait"
-                          />
-                        </div>
-                      </div>
-                      <div className="contrast-column">
-                        <div className="contrast-row">
-                          <img
-                            src={Landing_page_contrast}
-                            alt="Contrast "
-                            className="section__image--contrast"
-                          />
-                          <h2 className="section__text--intro-1 ui heading size-texts">Introduction to vehicle training</h2>
-                        </div>
-                        <img
-                          src={Landing_page_man_safety_equipment_work}
-                          alt="Safety Equipment "
-                          className="section__image--safety"
-                        />
-                      </div>
-                      <div className="success-row">
-                        <img
-                          src={Landing_page_success}
-                          alt="Success  One"
-                          className="section__image--success"
-                        />
-                        <h3 className="section__text--reduced-1 ui heading size-texts">Reduced Delivery Time</h3>
-                      </div>
-                    </div>
-                    <img src={Landing_page_group_1} alt="Group  Two" className="section__image--group" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          
         </div>
 
         {/* <!-- features overview section --> */}
@@ -140,14 +139,14 @@ const Landing_page = () => {
                     </div>
                     <div className="woman-hands-row">
                       <img
-                        src={Landing_page_woman_hands_hol}
-                        alt="Woman Hands "
+                        src={img_woman_hands_hol}
+                        alt="Woman Hands Image"
                         className="section__image--hands"
                       />
                       <div className="overflow-menu-row">
                         <img
-                          src={Landing_page_overflow_menu}
-                          alt="Overflow Menu "
+                          src={img_overflow_menu}
+                          alt="Overflow Menu Image"
                           className="section__image--menu"
                         />
                         <h3 className="section__text--intro ui heading size-textxs">Introduction to vehicle training</h3>
@@ -156,13 +155,13 @@ const Landing_page = () => {
                   </div>
                   <div className="success-row-two">
                     <img
-                      src={Landing_page_success_teal_900}
-                      alt="Success Teal "
+                      src={img_success_teal_900}
+                      alt="Success Teal Image"
                       className="section__image--teal"
                     />
                     <h4 className="section__text--reduced ui heading size-textmd">Reduced Delivery Time</h4>
                   </div>
-                  <img src={Landing_page_close} alt="Close " className="section__image--close" />
+                  <img src={img_close} alt="Close Image" className="section__image--close" />
                 </div>
                 <div className="whatsapp-column-one">
                   <h5 className="section__text--whatsapp ui heading size-heading2xl">WhatsApp Learning</h5>
@@ -198,14 +197,14 @@ const Landing_page = () => {
           <div className="description-row">
             <div className="description-row-inner">
               <div className="image-column">
-                <img src={Landing_page_image_64} alt=" Sixty Four" className="section__image--64" />
+                <img src={img_image_64} alt="Image Sixty Four" className="section__image--64" />
               </div>
               <div className="column-one">
-                <img src={Landing_page_image_54} alt=" Fifty Four" className="section__image--54" />
+                <img src={img_image_54} alt="Image Fifty Four" className="section__image--54" />
                 <div className="trending-column">
                   <div className="trending-row">
                     <button className="section__icon--trending ui button white_a700_cc size-sm fill round">
-                      <img src={Landing_page_trending_up}/>
+                      <img src={img_trending_up}/>
                     </button>
                     <div className="improvement-column">
                       <h2 className="section__text--improvement ui heading size-headingxl">32% improvement</h2>
@@ -234,14 +233,14 @@ const Landing_page = () => {
           <div className="row-seven">
             <div className="description-row-inner">
               <div className="column-two">
-                <img src={Landing_page_image_62_1_2} alt=" Sixty Two" className="section__image--62" />
+                <img src={img_image_62_1_2} alt="Image Sixty Two" className="section__image--62" />
               </div>
               <div className="column-three">
-                <img src={Landing_page_image_55} alt=" Fifty Five" className="section__image--55" />
+                <img src={img_image_55} alt="Image Fifty Five" className="section__image--55" />
                 <div className="trending-column">
                   <div className="trending-row-two">
                     <button className="section__icon--trending ui button white_a700_cc size-sm fill round">
-                      <img src={Landing_page_trending_up} />
+                      <img src={img_trending_up} />
                     </button>
                     <div className="revenue-column">
                       <h5 className="section__text--increase ui heading size-headingxl">43% increase</h5>
@@ -264,17 +263,17 @@ const Landing_page = () => {
             <div className="vector-forty-eight-row-two">
               <div className="row-eight">
                 <img
-                  src={Landing_page_whatsapp_image_2023_10_06}
-                  alt="Whatsapp "
+                  src={img_whatsapp_image_2023_10_06}
+                  alt="Whatsapp Image"
                   className="section__image--whatsapp"
                 />
               </div>
               <div className="column-four">
-                <img src={Landing_page_image_59} alt=" Fifty Nine" className="section__image--59" />
+                <img src={img_image_59} alt="Image Fifty Nine" className="section__image--59" />
                 <div>
                   <div className="trending-row-two">
                     <button className="section__icon--trending ui button white_a700_cc size-sm fill round">
-                      <img src={Landing_page_trending_up} />
+                      <img src={img_trending_up} />
                     </button>
                     <div className="reduction-column">
                       <h1 className="section__text--improvement ui heading size-headingxl">57% Reduction</h1>
@@ -297,7 +296,7 @@ const Landing_page = () => {
           </div>
         </div>
       </div>
-    </div>
+     
     </>
   );
 }
